a587ed3b4f651fcb780b74c8c973a9bd36c1d49c
munyokii <munyoki912@gmail.com>
2025-07-14 13:25:13 +0300

    :sparkles: Convex Hull using Grahams Scan

---
+++ graham_scan.py
@@ -0,0 +1,30 @@
+import math
+
+
+def orientation(p, q, r):
+    """Return the orientation of the triplet (p, q, r).
+       0 -> collinear; >0 -> counterclockwise; <0 -> clockwise."""
+    return (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+
+def distance_sq(p, q):
+    """Return the squared Euclidean distance between points p and q."""
+    return (p[0] - q[0])**2 + (p[1] - q[1])**2
+
+def convex_hull(points):
+
+    # Find the point with the lowest y (and lowest x in case of tie)
+    start = min(points, key=lambda p: (p[1], p[0]))
+
+    # Sort points by polar angle with the start point
+    sorted_points = sorted(points, key=lambda p: (
+        math.atan2(p[1] - start[1], p[0] - start[0]), distance_sq(start, p)))
+
+    hull = []
+    for p in sorted_points:
+
+        # Remove points that would cause a right (clockwise) turn.
+        while len(hull) >= 2 and orientation(hull[-2], hull[-1], p) <= 0:
+            hull. Pop()
+        hull. Append(p)
+
+    return hull
